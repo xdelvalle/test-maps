93a4c742a1940e890b9d066566aafe0f59527232
Xavi del Valle <xavier.delvalle@idiada.com>
2022-12-15 10:12:36 +0100

    Marker icon NOK ActionSheet NOK Android 33 (S21 Ultra)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {GoogleMap, Marker} from '@capacitor/google-maps';
 import {environment} from '../../environments/environment';
-import {Geolocation} from '@capacitor/geolocation';
+import {ActionSheetController, ModalController, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,31 +10,30 @@ import {Geolocation} from '@capacitor/geolocation';
 })
 export class HomePage {
 
-  // @ts-ignore
-  @ViewChild('map', { static: false }) mapRef: ElementRef;
-  // @ts-ignore
-  map: GoogleMap;
+  @ViewChild('map', { static: false }) mapRef!: ElementRef;
+  map!: GoogleMap;
   markers: Marker[] = [];
 
     elements = [
       {
         "latitude": 40.05631946196514,
         "longitude": -3.8439747997882,
-        "name": "Element 1"
+        "name": "Marker 1"
       },
       {
         "latitude": 40.06631946196514,
         "longitude": -3.8539747997882,
-        "name": "Element 2"
+        "name": "Marker 2"
       },
       {
         "latitude": 40.07631946196514,
         "longitude": -3.8639747997882,
-        "name": "Element 3"
+        "name": "Marker 3"
       }
     ];
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController,
+              public toastController: ToastController) {}
 
   ionViewDidEnter() {
     this.createMap().then();
@@ -77,5 +76,69 @@ export class HomePage {
     }
 
     await this.map.addMarkers(this.markers);
+
+    await this.map.setOnMarkerClickListener(async () => {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Albums',
+        cssClass: 'my-custom-class',
+        buttons: [
+          {
+            text: 'Delete',
+            role: 'destructive',
+            icon: 'trash',
+            handler: () => {
+              console.log('Delete clicked');
+              this.presentToast('Delete clicked');
+            },
+          },
+          {
+            text: 'Share',
+            icon: 'share',
+            handler: () => {
+              console.log('Share clicked');
+              this.presentToast('Share clicked');
+            },
+          },
+          {
+            text: 'Play (open modal)',
+            icon: 'caret-forward-circle',
+            handler: () => {
+              console.log('Play clicked');
+              this.presentToast('Play clicked');
+            },
+          },
+          {
+            text: 'Favorite',
+            icon: 'heart',
+            handler: () => {
+              console.log('Favorite clicked');
+              this.presentToast('Favorite clicked');
+            },
+          },
+          {
+            text: 'Cancel',
+            icon: 'close',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+              this.presentToast('Cancel clicked');
+            },
+          },
+        ],
+      });
+      await actionSheet.present();
+
+      const { role } = await actionSheet.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
+
+    });
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+    });
+    toast.present();
   }
 }
